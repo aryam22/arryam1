efad54553784be1701a9e6bdc20470c9b890205e
aryam22 <113521820+aryam22@users.noreply.github.com>
2022-09-14 20:13:18 +0300

    Add files via upload

---
+++ netbook activity.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('hi, my name is aryam')
+
+
+# In[2]:
+
+
+x=4
+y=8
+print(x)
+
+
+# In[3]:
+
+
+a=False
+print(a)
+print(type(a))
+print(int(True))
+
+
+# In[4]:
+
+
+print(25/4)
+
+
+# In[5]:
+
+
+print(25//6)
+
+
+# In[6]:
+
+
+print(30+2*6)
+
+
+# In[9]:
+
+
+print((30*25)*5)
+
+
+# In[11]:
+
+
+tuple1=("226",3,8)
+tuple1
+print(tuple1[0])
+print(tuple1[1])
+print(tuple1[2])
+
+
+# In[12]:
+
+
+L=["Aryam Rafea", 31.8, 2002]
+L
+
+
+# In[ ]:
+
+
+
+
